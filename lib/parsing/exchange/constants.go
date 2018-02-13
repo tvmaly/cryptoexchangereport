194707ee18128ed99d9930d2fb086ad3ca5e6925
Tyson Maly <mail@tysonmaly.com>
2018-02-13 09:47:34 -0500

    exchange constants to keep consistent representation across code

---
+++ lib/parsing/exchange/constants.go
@@ -0,0 +1,11 @@
+// exchange package holds exchange specific data types and constants
+// the constants specifically define how we refer to exchange exchange as represented as a string
+package exchange
+
+const GDAX = "gdax"
+
+const BITZ = "bitz"
+
+const BINANCE = "binance"
+
+const BITFINEX = "bitfinex"
