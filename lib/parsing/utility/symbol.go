3bc0bac35c097475fa26e60687c18f40500e197f
Tyson Maly <mail@tysonmaly.com>
2018-02-13 09:31:42 -0500

    added a utility to normalize symbol

---
+++ lib/parsing/utility/symbol.go
@@ -0,0 +1,9 @@
+package utility
+
+import "strings"
+
+// NormalizeSymbol a placeholder that will let us adapt the system to
+// some standard symbology format. wrap all raw exchange symbol values used with a call to this
+func NormalizeSymbol(symbol string) string {
+	return symbol
+}
