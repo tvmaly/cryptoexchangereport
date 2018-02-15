c930cf86ad42a35e94e4d42de11ca5e6681479fe
Tyson Maly <mail@tysonmaly.com>
2018-02-15 08:29:56 -0500

    reorganized where constants will reside in project

--- lib/constants/exchange.go
+++ lib/constants/exchange.go
@@ -1,11 +1,34 @@
-// exchange package holds exchange specific data types and constants
-// the constants specifically define how we refer to exchange exchange as represented as a string
-package exchange
+// constants package holds specific data types and constants
+// used by the system
+package constants
 
-const GDAX = "gdax"
+// a set of constants to represent exchanges
+const BINANCE = "binance"
 
 const BITZ = "bitz"
 
-const BINANCE = "binance"
-
 const BITFINEX = "bitfinex"
+
+const BITHUMB = "bithumb"
+
+const BITSTAMP = "bitstamp"
+
+const BITTREX = "bittrex"
+
+const COINONE = "coinone"
+
+const GDAX = "gdax"
+
+const GEMINI = "gemini"
+
+const HITBTC = "hitbtc"
+
+const HUOBI = "huobi"
+
+const KRAKEN = "kraken"
+
+const LIQUI = "liqui"
+
+const OKEX = "okex"
+
+const POLONIEX = "poloniex"
