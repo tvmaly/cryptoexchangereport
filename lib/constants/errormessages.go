c61e8f0e4593fd15b8f244266e5b2903ea2913b8
Agmr <leha.slavchenko@mail.ru>
2018-02-16 08:53:58 -0500

    added some errors to json as an example, and wrote parser in errormessage.go

--- lib/constants/errormessages.go
+++ lib/constants/errormessages.go
@@ -1,3 +1,43 @@
 package constants
 
+import (
+    "encoding/json"
+    "io/ioutil"
+    "errors"
+    "log"
+)
 
+var errorLookup map[int]error 
+
+func init() {
+
+    type ErrorJson struct {
+        Code int `json:"code"`
+        Error string `json:"error"`
+    }
+
+    var errs []ErrorJson
+
+    raw, err := ioutil.ReadFile("./data/errors.json")
+    if err != nil {
+        log.Fatal(err.Error())
+    }
+
+    json.Unmarshal(raw, &errs)
+
+    for _, v := range errs {
+
+        err := errors.New(v.Error)
+
+        errorLookup[v.Code] = err
+    }
+}
+
+func Error (code int) error {
+    if errorLookup[code] != nil {
+        return errorLookup[code]
+    }
+
+    return nil
+
+}
